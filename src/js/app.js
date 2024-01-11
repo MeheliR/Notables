a000753afa9bc16fe662b1d577a65c94c345a128
MeheliR <meheli2022roy@gmail.com>
2024-01-12 00:52:06 +0530

    new notebook creating feature

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,12 @@
  * Module import
  */
 
-import { addEventOnElements, getGreetingMsg } from "./utils.js";
+import { 
+    addEventOnElements, 
+    getGreetingMsg, 
+    activeNotebook, 
+    makeElemEditable 
+} from "./utils.js";
 import { Tooltip } from "./components/Tooltip.js";
 
 /**
@@ -52,3 +57,58 @@ $greetElem.textContent = getGreetingMsg(currentHour);
 
 const /** {HTMLElement} */ $currentDateElen = document.querySelector('[data-current-date]');
 $currentDateElen.textContent = new Date().toDateString().replace(' ',',  ');
+
+
+/**
+ * Notebook create field
+ */
+const /** {HTMLElement} */ $sidebarList = document.querySelector('[data-sidebar-list]');
+const /** {HTMLElement} */ $addNotebookBtn = document.querySelector('[data-add-notebook]');
+
+/**
+ * Shows a notebook creation field in the sidebar when the "Add Notebook" button is clicked.
+ * The function dynamically adds a new notebook field element, makes it editable, and listens for
+ * the 'Enter' key to create a new notebook when pressed.
+ */
+const showNotebookField = function () {
+    const /** {HTMLElement} */ $navItem = document.createElement('div');
+    $navItem.classList.add('nav-item');
+
+    $navItem.innerHTML = `
+        <span class="text text-large" data-notebook-field></span>
+
+        <div class="state-layer"></div>
+    `;
+
+    $sidebarList.appendChild($navItem);
+
+    const /** {HTMLElement} */ $navItemField = $navItem.querySelector('[data-notebook-field]');
+
+    // Activates new created notebook and deactive the last one.
+    activeNotebook.call($navItem);
+
+    // Make notebook field content editable and focus
+    makeElemEditable($navItemField);
+
+    //When user press 'Enter' then create notebook
+    $navItemField.addEventListener('keydown', createNotebook);
+
+}
+
+$addNotebookBtn.addEventListener('click', showNotebookField);
+
+/**
+ * Create new notebook
+ * Creates a new notebook when the 'Enter' key is pressed while editing a notebook name field.
+ * The new notebook is stored in the database.
+ * 
+ * @param {KeyboardEvent} event - The keyboard event that triggered notebook creation.
+ */
+const createNotebook = function (event) {
+
+    if (event.key === 'Enter') {
+        
+        // Store new created notebook in database
+
+    }
+}
